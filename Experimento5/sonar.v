df06469d589bba66374339b1de061b65eb048240
Erick-DAS <erickdiogo2004@gmail.com>
2024-09-25 18:47:23 -0300

    Initial commit for exp5

---
+++ Experimento5/sonar.v
@@ -0,0 +1,12 @@
+module sonar (
+ input wire clock,
+ input wire reset,
+ input wire ligar,
+ input wire echo,
+ output wire trigger,
+ output wire pwm,
+ output wire saida_serial,
+ output wire fim_posição
+); 
+
+endmodule
